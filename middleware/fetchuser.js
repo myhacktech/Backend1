9dcc3f1a80e915fa996a43313bec2e89d03f1d22
VAIBHAV SRIVASTAVA <86517958+myhacktech@users.noreply.github.com>
2023-12-04 18:20:47 +0530

    Update fetchuser.js

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -8,7 +8,7 @@ const fetchuser = (req, res, next) => {
   const token = req.header("auth-token");
 
   if (!token) {
-    return res.status(401).send({ error: "Please use a valid token" });
+    return res.status(401).send({ error: "no token found dear" });
   }
   try {
     // here data is the payload(user_id) from which token is formed
@@ -26,7 +26,7 @@ const fetchuser = (req, res, next) => {
 
     next(); // then process the next step
   } catch (error) {
-    res.status(401).send({ error: "Please use a valid token" });
+    res.status(401).send({ error: "u used an invalid token" });
   }
 };
 
